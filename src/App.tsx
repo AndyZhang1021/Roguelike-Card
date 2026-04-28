7bada28dcb5ef359e74d8a7e1a3ac5da10774a96
AndyZhang1021 <weihaoaz@gmail.com>
2026-04-28 11:55:05 +1000

    remove mui, initializing issue bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,17 @@ import MapScreen from "./components/screens/map-screens"
 import EntryScreen from "./components/screens/entry-screen"
 import { useGameStore } from "./stores/game-store"
 
-function App() {
+const App = () => {
   const screen = useGameStore((s) => s.screen)
-
   return (
-    <div style={{ maxWidth: 480, margin: '0 auto', padding: 16 }}>
+    <>
       {screen === 'entry' && <EntryScreen />}
-      {screen === 'main' && <MainScreen />}
       {screen === 'map' && <MapScreen />}
       {screen === 'battle' && <BattleScreen />}
       {screen === 'reward' && <div>Reward — TODO</div>}
       {screen === 'gameover' && <div>Game Over — TODO</div>}
       {screen === 'win' && <div>You Win — TODO</div>}
-    </div>
+    </>
   )
 }
 
--- src/components/screens/entry-screen.tsx
+++ src/components/screens/entry-screen.tsx
@@ -2,12 +2,18 @@ import { useGameStore } from "../../stores/game-store.ts"
 import type { Card } from "../../types/game.ts"
 import { Box } from "@mui/material";
 import Background from "../../assets/entry-background.png";
+import StartButton from "../../assets/start-button.png";
 
 const EntryScreen = () => {
 
   return (
-    <Box>
-      <img src={Background} alt="background" className="w-screen h-screen" />
+    <Box sx={{ background: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
+      className="w-screen h-screen">
+        <Box className="flex items-center justify-center w-full h-full">
+          <button type="button" className="w-64 cursor-pointer" onClick={() => console.log('start')}>
+            <img src={StartButton} className="w-full object-contain" />
+          </button>
+        </Box>
     </Box>
   )
 }
